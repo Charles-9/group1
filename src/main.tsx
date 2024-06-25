8722f5736e58e40afa107ca45bd0ee752d1bea33
Charles Hartmann <cohartma@ucsc.edu>
2024-06-26 11:24:24 +1200

    main overall screendiv

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className='screenDiv'>
+      <App />
+    </div>
   </React.StrictMode>,
 )
