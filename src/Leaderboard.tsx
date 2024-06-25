e4dcbb256f6c91480665065020337978b8585881
nyel451 <nyel451@aucklanduni.ac.nz>
2024-06-26 09:46:29 +1200

    testing table

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from "./components/ui/button"
+import { Button } from "./components/ui/button";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "./components/ui/table"
+  
 
 const Leaderboard: React.FC = () => {
     return (
@@ -14,6 +24,26 @@ const Leaderboard: React.FC = () => {
             </ol>
             <Button>Click me</Button>
             <Link to="/"><h2>Back to home screen</h2></Link>
+            <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+
         </>
     );
 };
