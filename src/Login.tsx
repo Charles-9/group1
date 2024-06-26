2aac13a089c68c1ebf7621d47af80b1a7c03cc7e
Charles Hartmann <cohartma@ucsc.edu>
2024-06-27 09:52:05 +1200

    fixed login page styling

--- src/Login.tsx
+++ src/Login.tsx
@@ -6,21 +6,23 @@ import { Button } from "./components/ui/button"
 const Login: React.FC = () => {
     return (
         <>
-            <h1 className="center-text scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" >
-                Login
-            </h1>
-            <br />
-            <div className='screenDiv'>
-                <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
-                    <Input placeholder='email address' />
-                    <br />
-                    <Input placeholder='password'></Input>
-                    <br />
+            <div className='h-screen flex flex-col'>
+                <h1 className="center-text scroll-m-20 mt-8 text-4xl font-extrabold tracking-tight lg:text-5xl" >
+                    Login
+                </h1>
+                <br />
+                <div className='flex flex-col items-center flex-grow rounded-tl-[30px] rounded-tr-[30px] mt-2 custom-gradient' >
+                    <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+                        <Input className='mt-8' placeholder='email address' />
+                        <br />
+                        <Input placeholder='password'></Input>
+                        <br />
+                    </div>
+                    <Link to="/profile" style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button>Log in</Button>
+                    </Link>
+                    <Link to="/"><h2>Back to home screen</h2></Link>
                 </div>
-                <Link to="/profile" style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Button>Log in</Button>
-                </Link>
-                <Link to="/"><h2>Back to home screen</h2></Link>
             </div>
         </>
     );
