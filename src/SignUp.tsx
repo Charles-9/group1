cddf52bba61f5ec83aef6035eb5810166d31be35
CTherond505 <caleb.therond@mds.ac.nz>
2024-06-26 14:10:10 +1200

    Sign Up Green Box
    added green box to sign up page

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -7,12 +7,13 @@ const SignUp: React.FC = () => {
     return (
         <>
             <br />
-            <h1 className="center-text scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" >
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" style={{paddingTop: '30px', paddingBottom: '10px', paddingLeft: '40px'}}>
                 Sign up
             </h1>
             <br />
 
-            <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+            <div className='screenDiv flex flex-col items-center rounded-tl-[30px] rounded-tr-[30px]'>
+            <div style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '100px'}}>
                 <Input placeholder='email address'></Input>
                 <br />
                 <Input placeholder='password'></Input>
@@ -23,6 +24,7 @@ const SignUp: React.FC = () => {
             <Link to="/connecttohop" style={{ display: 'flex', justifyContent: 'center' }}>
                 <Button>Sign Up</Button>
             </Link>
+            </div>
         </>
     );
 };
