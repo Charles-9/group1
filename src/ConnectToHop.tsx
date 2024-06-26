80161bc861816db147cb1f6ac70df80e247ab12c
CTherond505 <caleb.therond@mds.ac.nz>
2024-06-26 13:55:53 +1200

    Connect Logo
    added at hop logo and position to the connect page

--- src/ConnectToHop.tsx
+++ src/ConnectToHop.tsx
@@ -7,12 +7,19 @@ const Leaderboard: React.FC = () => {
     return (
         <>
             <br />
-            <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
-                <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                    Connect to AT Hop to track your trips
-                </h2>
 
+            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', height: '200px'}}>
+            <h2 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl" style={{paddingLeft: '30px'}}>
+                Connect to AT Hop
+            </h2>
+            <h2 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl" style={{paddingLeft: '30px'}}>
+                to track your trips
+            </h2>
+            <br />
+            <img src={"https://i0.wp.com/www.greaterauckland.org.nz/wp-content/uploads/2015/09/AT-Logo.png?fit=512%2C512&ssl=1"} alt = "Logo" style={{width: '170px', alignSelf: 'flex-end', paddingRight: '40px'}}/>
+            </div>
 
+            <div style={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '50px'}}>
                 <Input placeholder='AT Hop number'></Input><br />
                 <Input placeholder='email address'></Input>
                 <br />
