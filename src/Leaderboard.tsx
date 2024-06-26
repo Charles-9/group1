de0278525e53e4494328c1a82d21f97726af1b27
Brook Pugmire <brook.p@icloud.com>
2024-06-26 14:44:07 +1200

    select variations on leaderboard

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LeaderBoardTable from "./components/LeadboardEntry"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
+
+
 
 
 const Leaderboard: React.FC = () => {
@@ -13,12 +16,39 @@ const Leaderboard: React.FC = () => {
     ]);
 
     const sortedEntries = [...entries].sort((a, b) => b.points - a.points);
+    const [selectedTab, setSelectedTab] = useState('all-time');
 
     return (
         <div className='screenDiv'>
             <h1>Leaderboard</h1>
             <Link to="/"><h2>Back to home screen</h2></Link>
             <LeaderBoardTable entries={sortedEntries} />
+
+            <Tabs defaultValue="account" className="p-1 bg-gray-100" >
+            <TabsList className='flex justify-around'>
+                    <TabsTrigger
+                        value="all-time"
+                        className={`data-[state=active]:text-secondary data-[state=active]:bg-white text-gray-500 hover:text-primary ${selectedTab === 'all-time' ? 'text-secondary' : ''}`}
+                        onClick={() => setSelectedTab('all-time')}
+                    >
+                        All time
+                    </TabsTrigger>
+                    <TabsTrigger
+                        value="month"
+                        className={`data-[state=active]:text-secondary data-[state=active]:bg-white text-gray-500 hover:text-primary ${selectedTab === 'month' ? 'text-secondary' : ''}`}
+                        onClick={() => setSelectedTab('month')}
+                    >
+                        This Month
+                    </TabsTrigger>
+                    <TabsTrigger
+                        value="week"
+                        className={`data-[state=active]:text-secondary data-[state=active]:bg-white text-gray-500 hover:text-primary ${selectedTab === 'week' ? 'text-secondary' : ''}`}
+                        onClick={() => setSelectedTab('week')}
+                    >
+                        This Week
+                    </TabsTrigger>
+                </TabsList>
+            </Tabs>
         </div>
     )
 }
